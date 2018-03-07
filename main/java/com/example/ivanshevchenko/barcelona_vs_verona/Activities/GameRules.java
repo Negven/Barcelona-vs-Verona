aea2b91ae1eaff874efeb52b9417a0de3367389a
Gavgavmeow <shevchenkoivan2003@gmail.com>
2018-03-07 16:00:54 +0200

    Добавил украинскую версию карточек. У кого будет время переведите их пожалуйста на англиский и русскийЭ

--- main/java/com/example/ivanshevchenko/barcelona_vs_verona/Activities/GameRules.java
+++ main/java/com/example/ivanshevchenko/barcelona_vs_verona/Activities/GameRules.java
@@ -1,7 +1,9 @@
-package com.example.ivanshevchenko.barcelona_vs_verona;
+package com.example.ivanshevchenko.barcelona_vs_verona.Activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+
+import com.example.ivanshevchenko.barcelona_vs_verona.R;
 
 public class GameRules extends Activity {
 
--- main/java/com/example/ivanshevchenko/barcelona_vs_verona/Activities/MainMenu.java
+++ main/java/com/example/ivanshevchenko/barcelona_vs_verona/Activities/MainMenu.java
@@ -1,10 +1,12 @@
-package com.example.ivanshevchenko.barcelona_vs_verona;
+package com.example.ivanshevchenko.barcelona_vs_verona.Activities;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.ivanshevchenko.barcelona_vs_verona.R;
 
 public class MainMenu extends Activity {
 
